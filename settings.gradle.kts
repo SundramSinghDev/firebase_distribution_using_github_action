6ccbd85a09c4cdc74cb7e01e935f71db75464059
SundramSinghDev <sudramsingh@cedcommerce.com>
2024-01-22 12:42:35 +0530

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "firebase_distribution_using_github_action"
+include(":app")
